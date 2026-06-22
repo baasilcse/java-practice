fe2fe2631417d39311cde96179279fb3bf01a1f7
moham <moham@MOHAMED-BAASIL>
2026-06-22 21:47:58 +0530

    today ,linked list

--- Java_Practice_45/src/days45/Main.java
+++ Java_Practice_45/src/days45/Main.java
@@ -4,6 +4,14 @@ public class Main {
     public static void main(String[] args) {
     	linkedList l=new linkedList();
     	l.insert(11);
-    	
+    	l.insert(12);
+    	l.insert(14);
+    	l.insert(15);
+    	//l.insertAtBegining(10);
+    	l.insertAtPosn(13,1);
+    	//l.insert(12);
+    	//l.deleteByValue(11);
+    	//l.deleteAtBegining();
+    	l.display();
     }
 }
--- Java_Practice_45/src/days45/linkedList.java
+++ Java_Practice_45/src/days45/linkedList.java
@@ -1,29 +1,66 @@
 package days45;
 
 public class linkedList {
-	node first;
-	node last;
-	
+	node first; //head
+	node last;   //tail
 	linkedList(){
 		first=null;
 		last=null;
 	}
-	
 	void insert(int data) {
 		node newnode =new node(data);
 		if(first==null) {
 			first=last=newnode;	
-		}
+			return;
+		}           
 		last.next=newnode;
 		last=newnode;
 	}
 	
+	void insertAtBegining(int data) {
+		node newnode=new node(data);
+		newnode.next=first;
+		first=newnode;
+	}
+	
+	void insertAtPosn(int data,int posn) {
+	    if(posn==1) {
+	    	insertAtBegining(data);
+	    	return;
+	    }
+		node temp=first;
+		node newnode =new node(data);
+		for(int i=1;i<=posn-2;i++) {  // pos-2 because to insert at posn n,we need to traverse before the 2 iteration before
+			temp=temp.next;
+		}
+		newnode.next=temp.next;          // first we updated the newnode's next then we update the temp's next (because if we changed the temp'next first ,then reference for rightside node is lost  )
+		temp.next=newnode;
+	}
+	
+	void deleteAtBegining() {
+		first=first.next;
+	}
+	
+	void deleteByValue(int val) {
+		node temp=first;
+		 
+		if(temp!=null && temp.data==val) {
+			first=temp.next;
+			return;
+		}
+		while(temp!=null) {
+			if(temp.next.data==val) {
+				temp.next=temp.next.next;
+				break;
+			}
+			temp=temp.next;
+		}
+	}
 	void display() {
 		node temp=first;
 		while(temp!=null) {
-			System.out.println(temp.data);
+			System.out.print(temp.data+" ");
 			temp=temp.next;
-		}
-		
+		}	
 	}
 }
